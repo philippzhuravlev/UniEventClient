4f33fa20f994b6a116b651d0dacce43d63f483e2
Christian Seestern Hyllested <crillerhylle@gmail.com>
2026-04-15 23:37:49 +0200

    updated FB Redirect URI

--- web/src/services/facebook.ts
+++ web/src/services/facebook.ts
@@ -1,9 +1,10 @@
 export function buildFacebookLoginUrl() {
   const FB_APP_ID = import.meta.env.VITE_FACEBOOK_APP_ID;
-  const FB_REDIRECT_URI = encodeURIComponent('https://europe-west1-dtuevent-8105b.cloudfunctions.net/handleCallback');
+  const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8080';
+  const FB_REDIRECT_URI = encodeURIComponent(`${BACKEND_BASE_URL}/api/facebook/callback`);
   const FB_SCOPES = [
     'pages_show_list',
     'pages_read_engagement'
   ].join(',');
-  return `https://www.facebook.com/v23.0/dialog/oauth?client_id=${FB_APP_ID}&redirect_uri=${FB_REDIRECT_URI}&scope=${FB_SCOPES}`;
+  return `https://www.facebook.com/v25.0/dialog/oauth?client_id=${FB_APP_ID}&redirect_uri=${FB_REDIRECT_URI}&scope=${FB_SCOPES}`;
 }
